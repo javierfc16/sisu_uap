09690fbc4ecdb4b47e0257b6a04aa12f4424ea79
javierfc16 <javierfc1611@gmail.com>
2023-10-05 09:17:01 -0400

    update1

---
+++ src/test/java/com/example/sisu/SisuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sisu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
